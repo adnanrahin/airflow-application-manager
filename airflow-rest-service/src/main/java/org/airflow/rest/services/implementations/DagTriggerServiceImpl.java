91c0e8196933066a7f7bb27202aff430099812d2
Hamidur Rahman <Hamidur0405@gmail.com>
2023-11-07 23:40:33 -0500

    dag runs

--- airflow-rest-service/src/main/java/org/airflow/rest/services/implementations/DagTriggerServiceImpl.java
+++ airflow-rest-service/src/main/java/org/airflow/rest/services/implementations/DagTriggerServiceImpl.java
@@ -19,7 +19,7 @@ public class DagTriggerServiceImpl implements DagTriggerService {
     @Override
     public void triggerDag(String dagId) throws JsonProcessingException {
 
-        String airflowApiUrl = "http://192.168.1.235:18080/api/v1/dags/{dagId}/dagRuns";
+        String airflowApiUrl = "";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -45,6 +45,48 @@ public class DagTriggerServiceImpl implements DagTriggerService {
         System.out.println(state);
         System.out.println(dagRunId);
 
+        isDagRunning(dagId);
+
+    }
+
+    boolean isDagRunning(String dagId) throws JsonProcessingException {
+
+
+        String airflowApiUrl = "";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String authHeader = "Basic " + Base64.getEncoder().encodeToString("admin:admin".getBytes());
+        headers.set("Authorization", authHeader);
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> request = new HttpEntity<>("{}", headers);
+
+        restTemplate.postForEntity(airflowApiUrl, request, String.class, dagId);
+
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(airflowApiUrl, request, String.class, dagId);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode root = objectMapper.readTree(responseEntity.getBody());
+
+        int failed = 0;
+        int success = 0;
+        int running = 0;
+        int queued = 0;
+
+        System.out.println(root.size());
+
+        JsonNode dagRuns = root.path("dag_runs");
+
+        for (JsonNode dagRun : dagRuns) {
+            String state = dagRun.get("state").textValue();
+
+            if (state.trim().equalsIgnoreCase("running"))
+                return true;
+        }
+
+        return false;
     }
 
 }
