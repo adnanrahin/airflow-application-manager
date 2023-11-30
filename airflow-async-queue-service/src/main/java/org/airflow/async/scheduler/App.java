a0bec1e66ce766ff6976d276531b3763de2a19b7
adnanrahin <adnan.rahin13@gmail.com>
2023-11-30 02:36:28 -0500

    airflow task queue module added

---
+++ airflow-async-queue-service/src/main/java/org/airflow/async/scheduler/App.java
@@ -0,0 +1,10 @@
+package org.airflow.async.scheduler;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
